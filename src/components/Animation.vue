5579006bcaeb64a178de57addaf889126598d8d4
rchaser53 <tayoshizawa29@gmail.com>
2017-05-20 17:33:16 +0900

    use two transition

--- src/components/Animation.vue
+++ src/components/Animation.vue
@@ -2,10 +2,13 @@
   <div class="hello">
     <button v-on:click="show = !show">
       Toggle
-    </button>    
-    <transition name="fade">
-      <p v-if="show">hello</p>
-    </transition>
+    </button>
+      <transition name="fade">
+        <p class="nyan" v-if="show">hello</p>
+      </transition>
+    <div class="outer">
+
+    </div>
   </div>
 </template>
 
@@ -20,17 +23,31 @@ export default {
 </script>
 
 <style scoped>
+  .outer {
+    display: block;
+    position: relative;
+    height: 30px;
+    width: 500px;
+  }
+
   .hello {
     --test-opacity: 0;
   }
   .fade-enter-active, .fade-leave-active {
-    transition: transform 1.5s ease;
+    transition: left 1.5s ease, opacity 1s ease-in;
+  }
+
+  .nyan {
+    position: absolute;
+    left: 150px;
   }
 
   .fade-enter {
+    left: 300px;
     opacity: var(--test-opacity);
   }
   .fade-leave-to {
+    left: -50px;
     opacity: var(--test-opacity);
   }
     
