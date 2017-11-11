d4a8e2a856947b7852591e05f8ed80ef5387ab71
rchaser53 <tayoshizawa29@gmail.com>
2017-11-11 20:19:09 +0900

    try to use position:sticky to fixed top

---
+++ src/components/Sticky.vue
@@ -0,0 +1,39 @@
+<template>
+  <div class="outer">
+    <div class="sticky">hoge</div>
+    <div class="backbone"></div>
+  </div>
+
+
+</template>
+
+<script lang="ts">
+export default {
+  name: 'sticky',
+  data () {
+    return {}
+  }
+}
+</script>
+
+<style scoped>
+  .outer {
+    display:block;
+    width: 500px;
+    height: 500px;
+    overflow: scroll;
+  }
+  .backbone {
+    display:block;
+    width: 500px;
+    height: 700px;
+    background-color:aqua;
+  }
+  .sticky {
+    color: blue;
+    position: sticky;
+    background-color: yellow;
+    display: block;
+    top: 0px;
+  }
+</style>
