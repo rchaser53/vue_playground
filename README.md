cee83b3c39be13e0edaa9fbb2f203aaeb705c568
rchaser53 <tayoshizawa29@gmail.com>
2020-01-27 23:27:53 +0900

    rebuid

--- README.md
+++ README.md
@@ -1,7 +1,24 @@
-# vueplayground
+# vue-playground
 
-## Quick Run
-1. yarn install
-2. node server
-3. npm run dev
-4. open browswer 'localhost:8080/element'
+## Project setup
+```
+yarn install
+```
+
+### Compiles and hot-reloads for development
+```
+yarn serve
+```
+
+### Compiles and minifies for production
+```
+yarn build
+```
+
+### Lints and fixes files
+```
+yarn lint
+```
+
+### Customize configuration
+See [Configuration Reference](https://cli.vuejs.org/config/).
--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,25 @@
 <template>
   <div id="app">
-    <router-view></router-view>
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
   </div>
 </template>
 
 <script lang="ts">
-  export default {
-    name: 'app'
+import Vue from "vue";
+import HelloWorld from "./components/HelloWorld.vue";
+
+export default Vue.extend({
+  name: "app",
+  components: {
+    HelloWorld
   }
+});
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
